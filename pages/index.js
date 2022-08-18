ad8fdc702cd88d0c27d19b974aba29b7ae26dfaa
59417 <sun313131@gmail.com>
2022-08-18 21:03:21 +0800

    Modify start component path

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-import Start from '../components/Start/Start';
+import Start from '../components/start/Start';
 
 export default function Home() {
   return (
